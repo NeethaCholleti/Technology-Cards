1f584e2d4577d70ac4dccb03a7186c043fb85daa
NeethaCholleti <neetha.ch94@gmail.com>
2023-07-21 13:56:54 +0000

    Techcards

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,9 @@
+// Write your code here.
+import './index.css'
+
+const CardDetails = props => {
+  const {cardDetails} = props
+  //console.log(cardDetails)
+  const {title, description, imgUrl} = cardDetails
+}
+export default CardDetails
