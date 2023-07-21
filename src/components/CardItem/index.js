27124489ceabf7e35d593785b72d2be49228fbd0
NeethaCholleti <131853060+NeethaCholleti@users.noreply.github.com>
2023-07-21 19:47:20 +0530

    Update index.js

--- src/components/CardItem/index.js
+++ src/components/CardItem/index.js
@@ -4,6 +4,14 @@ import './index.css'
 const CardDetails = props => {
   const {cardDetails} = props
   //console.log(cardDetails)
-  const {title, description, imgUrl} = cardDetails
+  const {title, description, imgUrl, className} = cardDetails
+
+  return (
+    <li className={`${className} cardContainer`}>
+      <h1 className="head"> {title} </h1>
+      <p className="para">{description}</p>
+      <mg src={imgUrl} className="image" alt={title} />
+    </li>
+  )
 }
 export default CardDetails
